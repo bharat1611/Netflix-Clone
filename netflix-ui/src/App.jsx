2f07c274ef476f86d3a7b6349c788f0c4bf61fa9
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2024-05-13 20:56:15 +0530

    updates

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Netflix from './pages/Netflix'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import "./index.css"
 
 export default function App() {
   return (
